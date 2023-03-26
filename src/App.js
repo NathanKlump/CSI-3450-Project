7610e571aa1fbafef20bcba12c7fbadcc5b21608
NathanKlump <98673875+NathanKlump@users.noreply.github.com>
2023-03-26 12:59:12 -0400

    add form submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    address: '',
+    squareFootSize: '',
+    bedrooms: '',
+    bathrooms: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>House Information Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Address:
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Square Foot Size:
+          <input
+            type="number"
+            name="squareFootSize"
+            value={formData.squareFootSize}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Bedrooms:
+          <input
+            type="number"
+            name="bedrooms"
+            value={formData.bedrooms}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Bathrooms:
+          <input
+            type="number"
+            name="bathrooms"
+            value={formData.bathrooms}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
